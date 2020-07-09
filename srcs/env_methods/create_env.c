976408b40d4582700a9691b9386f9e03fb91e52a
Jeremie <jeremiegthr@gmail.com>
2020-07-09 08:05:18 +0200

    increment SHLVL onstart

--- srcs/env_methods/create_env.c
+++ srcs/env_methods/create_env.c
@@ -6,13 +6,29 @@
 /*   By: jergauth <jergauth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 11:43:11 by jergauth          #+#    #+#             */
-/*   Updated: 2019/11/12 11:43:35 by jergauth         ###   ########.fr       */
+/*   Updated: 2020/07/09 08:03:49 by jergauth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		create_env(t_shell *shell, char **const env)
+static void increment_shell_level(t_shell *shell)
+{
+  char *shell_lvl_content;
+  int   shell_lvl;
+
+  if ((shell_lvl_content = get_var_content("SHLVL", shell->env)))
+  {
+    shell_lvl = ft_atoi(shell_lvl_content) + 1;
+    if ((shell_lvl_content = ft_itoa(shell_lvl)))
+    {
+      upsert_env(shell, "SHLVL", shell_lvl_content);
+      ft_strdel(&shell_lvl_content);
+    }
+  }
+}
+
+int	  	    create_env(t_shell *shell, char **const env)
 {
 	size_t	i;
 
@@ -28,5 +44,6 @@ int		create_env(t_shell *shell, char **const env)
 		i++;
 	}
 	shell->env_idx = i;
+  increment_shell_level(shell);
 	return (0);
 }
