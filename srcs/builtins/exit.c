415053c905a215943b90417e9aaaa531ffd8d77c
Jeremie <jeremiegthr@gmail.com>
2020-07-09 08:15:06 +0200

    exit can now take a status_code in param

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: jergauth <jergauth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 13:13:36 by jergauth          #+#    #+#             */
-/*   Updated: 2019/11/19 11:38:06 by jergauth         ###   ########.fr       */
+/*   Updated: 2020/07/09 08:11:11 by jergauth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 int		exit_builtin(t_shell *shell)
 {
 	shell->status = STOPPED;
-	shell->exps.last_exit_status = 0;
+  if (shell->argv[1])
+    shell->exps.last_exit_status = ft_atoi(shell->argv[1]);
+  else
+	  shell->exps.last_exit_status = 0;
 	return (0);
 }
